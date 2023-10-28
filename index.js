0e717a60efee5ed44d39da32eb159421dfccaac5
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-10-28 13:03:04 +0300

    fix

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const router = require('./routes/router');
 const mongoose = require('mongoose');
 require('dotenv/config');
 
